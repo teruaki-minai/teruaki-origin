cb4b4e47d3145f8c485f23e696842eb92de72a4d
t.minai <seisi0wakwbl.ue@gmail.com>
2019-02-25 02:45:16 +0000

    toppage Using?

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,6 +1,6 @@
 <header class="mb-4">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark"> 
-        <a class="navbar-brand" href="/">Microposts</a>
+        <a class="navbar-brand" href="/">Using?</a>
          
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
@@ -9,8 +9,8 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Sign up</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Sign in</a></li>
             </ul>
         </div>
     </nav>
